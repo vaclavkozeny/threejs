d6a9652e08104dfc4f9834d4548d7d392e580bec
vaclavkozeny <vackoze019@pslib.cz>
2023-10-05 09:41:21 +0200

    update

--- main.js
+++ main.js
@@ -7,26 +7,29 @@ const renderer = new THREE.WebGLRenderer();
 renderer.setSize( window.innerWidth, window.innerHeight );
 document.body.appendChild( renderer.domElement );
 
-const geometry = new THREE.BoxGeometry( 1, 1, 1 );
-const geometry2 = new THREE.TorusGeometry( 10, 3, 16, 100 );
-const material = new THREE.MeshBasicMaterial( { color: 0x00ffff } );
+const geometry2 = new THREE.TorusGeometry( 10, 1, 16, 100 );
+const geometry3 = new THREE.TorusGeometry( 5, 1, 16, 100 );
+const material = new THREE.MeshBasicMaterial( { color: 0x0000ff } );
 const material2 = new THREE.MeshBasicMaterial( { color: 0xff0000 } );
-const cube = new THREE.Mesh( geometry, material );
 const torus = new THREE.Mesh(geometry2, material2);
+const torus2 = new THREE.Mesh(geometry3, material);
+const light = new THREE.AmbientLight( 0x404040 ); // soft white light
+scene.add( light );
 renderer.setClearColor( 0xffffff );
-scene.add(cube);
+
 scene.add(torus);
+scene.add(torus2);
 
 camera.position.z = 30;
 
 function animate() {
     requestAnimationFrame( animate );
 
-    cube.rotation.x += 0.01;
-    cube.rotation.y += 0.05;
-    torus.rotation.x += 0.01;
-    torus.rotation.y += 0.01;
-    renderer.render( scene, camera );
+    torus.rotation.x += 0.005;
+    torus.rotation.y += 0.005;
+    torus2.rotation.x -= 0.01;
+    torus2.rotation.y -= 0.01;
+    renderer.render( scene, camera, light );
 }
 
 animate();
